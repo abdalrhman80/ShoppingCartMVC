d978491d6249ba1abd58a0e175063438390fe616
Abdalrhman Gamal <abdalrhmangamel681@gmail.com>
2024-11-16 03:59:13 +0200

    RenameAspNetUserLoginsTable

---
+++ ShoppingCart.DAL/Migrations/20241116014530_RenameAspNetUserLoginsTable.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingCart.DAL.Migrations
+{
+    public partial class RenameAspNetUserLoginsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetUserLogins",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserLogins",
+                newName: "UserLogins");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserLogins",
+                table: "UserLogins",
+                columns: new[] { "LoginProvider", "ProviderKey" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserLogins",
+                table: "UserLogins");
+
+            migrationBuilder.RenameTable(
+                name: "UserLogins",
+                newName: "AspNetUserLogins");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetUserLogins",
+                table: "AspNetUserLogins",
+                columns: new[] { "LoginProvider", "ProviderKey" });
+        }
+    }
+}
